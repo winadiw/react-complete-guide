f239e5e124ed277804029e785e73f5bd92422a93
winadilaku6 <winadi@laku6.com>
2018-07-16 11:07:17 +0700

    video #91 - create AUX element as higher order component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 const cockpit = (props) => {
 
     let assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
     if(props.showPersons) {
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(' ');
     }
 
     if (props.persons.length <= 2) {
@@ -18,13 +19,13 @@ const cockpit = (props) => {
     }
 
     return (
-            <div className={classes.Cockpit}>
-                <h1>{props.appTitle}</h1>
-                <p className={assignedClasses.join(' ')}>This is really working!</p>
-                <button
-                className = {btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
-            </div>
+        <Aux>
+            <h1>{props.appTitle}</h1>
+            <p className={assignedClasses.join(' ')}>This is really working!</p>
+            <button
+            className = {btnClass}
+            onClick={props.clicked}>Toggle Persons</button>
+        </Aux>
     );
 }
 
